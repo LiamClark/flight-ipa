2b7991906b4360a2058d0d80c02ba85a1a8e1267
LiamClark <L.A.Clark@student.tudelft.nl>
2023-05-11 18:50:27 +0200

    Create new component that fetches cached or live flight api data

---
+++ app/Flight.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+
+const testing = true
+
+function loadData() {
+    const url = testing ? "http://localhost:3000/flight.json" : "https://opensky-network.org/api/states/all"
+    return fetch(url)
+        .then(r => r.json())
+        // debugger
+}
+
+export default function Flight() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        loadData()
+            .then(j => {
+                console.log(j)
+                return setData(j)
+            })
+    }, [data, setData])
+
+    return (<h1>{data ? data.time : "null"}</h1>)
+}
