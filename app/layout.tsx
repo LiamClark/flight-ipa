d8cfa414398a0319a39e3437aa824af7d2d0d441
LiamClark <L.A.Clark@student.tudelft.nl>
2023-05-16 20:28:22 +0200

    Create dummy menu header + dashboard area

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html class="h-full bg-gray-100" lang="en">
+      <body className=  { 'h-full ' + inter.className}>{children}</body>
     </html>
   )
 }
